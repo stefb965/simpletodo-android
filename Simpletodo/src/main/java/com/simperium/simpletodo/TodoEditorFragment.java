fd4e26099361372505742c58185967c9c7a78270
Dan Roundhill <dan@automattic.com>
2015-08-27 11:59:19 -0700

    Code formatting cleanup

--- Simpletodo/src/main/java/com/simperium/simpletodo/TodoEditorFragment.java
+++ Simpletodo/src/main/java/com/simperium/simpletodo/TodoEditorFragment.java
@@ -104,18 +104,7 @@ public class TodoEditorFragment extends DialogFragment implements TextView.OnEdi
         return true;
     }
 
-    /**
-     * This interface must be implemented by activities that contain this
-     * fragment to allow an interaction in this fragment to be communicated
-     * to the activity and potentially other fragments contained in that
-     * activity.
-     * <p>
-     * See the Android Training lesson <a href=
-     * "http://developer.android.com/training/basics/fragments/communicating.html"
-     * >Communicating with Other Fragments</a> for more information.
-     */
     public interface OnTodoEditorCompleteListener {
         void onTodoEdited(String key, String label);
     }
-
 }
